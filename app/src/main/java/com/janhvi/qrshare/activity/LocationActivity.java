9017395fea58c29016f1f4c98ee0cd8da9a4f48c
janhvi <janhvibaraskar03@gmail.com>
2025-06-10 00:17:34 +0530

    Added activities

---
+++ app/src/main/java/com/janhvi/qrshare/activity/LocationActivity.java
@@ -0,0 +1,21 @@
+package com.janhvi.qrshare.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.janhvi.qrshare.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+    }
+}
---
+++ app/src/main/java/com/janhvi/qrshare/activity/SmsActivity.java
@@ -0,0 +1,16 @@
+package com.janhvi.qrshare.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.janhvi.qrshare.R;
+
+public class SmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+
+    }
+}
